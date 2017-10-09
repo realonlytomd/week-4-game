47d303b6d439c980ea05026cae8af0cf92849df9
realonlytomd <mclaughlintomd@gmail.com>
2017-10-09 13:22:13 -0500

    made crystals buttons

---
+++ assets/javascript/games.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+    $("#btnBlue")
+        .text("")
+        .append("<img src=../images/blue.jpg alt=blue width=100px height=100px>")
+        .button();
+ });
+
+   
